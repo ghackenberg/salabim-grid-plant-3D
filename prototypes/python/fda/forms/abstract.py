761c54100640dc9ae208a3c55fe4b621dafac294
Georg Hackenberg <ghackenberg@gmail.com>
2022-12-07 18:09:22 +0100

    more simple implementation of text change

--- prototypes/python/fda/forms/abstract.py
+++ prototypes/python/fda/forms/abstract.py
@@ -2,9 +2,9 @@ from typing import TypeVar
 from typing import Generic
 from tkinter import Frame
 from tkinter import Misc
-from tkinter import StringVar
 from tkinter import Label
 from tkinter import Entry
+from tkinter import Event
 from tkinter import END
 from ..eventbus import EventBus
 from ..objects import AbstractObject
@@ -30,15 +30,13 @@ class AbstractForm(Frame, Generic[T]):
         self.defaultView.grid(row=0, column=0, sticky='')
         # Container for form elements
         self.formView = Frame(self)
-        # String variable for the object name
-        self.nameVar = StringVar(self.formView)
-        self.nameVar.trace('w', lambda a, b, c: self.handleNameChange())
         # Label for text input field for object name
         self.nameLabel = Label(self.formView, text='Name:')
         self.nameLabel.grid(row=0, column=0, padx=5, pady=5)
         # Text input field for changing the name
-        self.nameEntry = Entry(self.formView, textvariable=self.nameVar)
+        self.nameEntry = Entry(self.formView)
         self.nameEntry.grid(row=0, column=1, padx=5, pady=5)
+        self.nameEntry.bind('<FocusOut>', self.handleNameChange)
     
     def setObject(self, object: T):
         # Remember the new object, which is shown in the form
@@ -49,16 +47,17 @@ class AbstractForm(Frame, Generic[T]):
             self.defaultView.grid_forget()
             self.formView.grid(row=0, column=0, sticky='')
             # Update value of name input field
-            self.nameVar.set(object.name)
+            self.nameEntry.delete(0, END)
+            self.nameEntry.insert(0, object.name)
         else:
             # Switch view
             self.formView.grid_forget()
             self.defaultView.grid(row=0, column=0, sticky='')
 
-    def handleNameChange(self):
+    def handleNameChange(self, event: Event):
         # Check if names are different first!
-        if self.object.name != self.nameVar.get():
+        if self.object.name != self.nameEntry.get():
             # Update name
-            self.object.name = self.nameVar.get()
+            self.object.name = self.nameEntry.get()
             # Propagate event
             self.eventbus.emit(f'{self.prefix}-update', self.object)
