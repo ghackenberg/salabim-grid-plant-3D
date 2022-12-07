13297c9a7b224c1d45ce18152c692fefe8b14609
Georg Hackenberg <ghackenberg@gmail.com>
2022-12-07 08:42:23 +0100

    introduce application class

---
+++ prototypes/python/fda/editors/layouts.py
@@ -0,0 +1,5 @@
+from tkinter import Frame
+
+class LayoutsEditor(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
--- prototypes/python/main.py
+++ prototypes/python/main.py
@@ -1,26 +1,4 @@
-from tkinter import Tk
-from tkinter import BOTH
-from tkinter.ttk import Notebook
-from editors.tools import ToolsEditor
-from editors.machines import MachinesEditor
-from editors.products import ProductsEditor
-from editors.scenarios import ScenariosEditor
+from fda.application import Application
 
-root = Tk()
-root.title('Factory Design Automation (FDA) Tool')
-root.attributes('-zoomed', True)
-
-notebook = Notebook(root)
-
-tools = ToolsEditor(notebook)
-machines = MachinesEditor(notebook)
-products = ProductsEditor(notebook)
-scenarios = ScenariosEditor(notebook)
-
-notebook.add(tools, text='Tools')
-notebook.add(machines, text='Machines')
-notebook.add(products, text='Products')
-notebook.add(scenarios, text='Scenarios')
-notebook.pack(expand=1, fill=BOTH)
-
-root.mainloop()
+application = Application()
+application.mainloop()
