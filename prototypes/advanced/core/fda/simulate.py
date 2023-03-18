01c2e5aa543f6742f6c56ab9b156b94a76bbfa51
Sammarcochiara <chiara.sammarco99@gmail.com>
2023-03-18 12:30:07 +0100

    Simulate model: graphical modification

--- prototypes/advanced/core/fda/simulate.py
+++ prototypes/advanced/core/fda/simulate.py
@@ -2,7 +2,9 @@ import salabim as sim
 
 from .model import *
 
+
 def simulate(layout: Layout, scenario: Scenario):
+
     #General visualization commands
     env = sim.Environment()
 
@@ -20,10 +22,11 @@ def simulate(layout: Layout, scenario: Scenario):
     env.show_camera_position(True)
     env.show_camera_position(over3d=True)
 
-    env.view(x_eye=5, y_eye=15, z_eye=7.5)
-
+    env.view(x_eye=0, y_eye=15, z_eye=5)
 
     sim.Animate3dGrid(x_range=range(-10, 10), y_range=range(-10, 10))
+
+
 
     #Transversal corridors counting
     corridor_count = len(layout.corridors) # numbers of t_corridors in a certain layout
@@ -31,14 +34,14 @@ def simulate(layout: Layout, scenario: Scenario):
 
 
     # Draw backbone (main corridor)
-    sim.Animate3dBox(x_len=0.5, y_len=y*2.13, z_len=0.25, color="red", x=0, y=0, z=2.5)
+    sim.Animate3dBox(x_len=0.25, y_len=y*2.06, z_len=0.25, color="red", x=0, y=0, z=2.5)
 
     # Down (connection robot-2 storage areas)
     sim.Animate3dBox(x_len=0.25, y_len=0.25, z_len=1.5, color="red", x=0, y=y, z=1.625)
     sim.Animate3dBox(x_len=0.25, y_len=0.25, z_len=1.5, color="red", x=0, y=-y, z=1.625)
 
     # Robot
-    sim.Animate3dBox(x_len=0.5, y_len=0.5, z_len=0.5, color="red", x=0, y=0, z=2.5)
+    sim.Animate3dBox(x_len=0.5, y_len=0.5, z_len=0.5, color="red", edge_color= 'white', x=0, y=0, z=2.5)
 
     # Storage Areas in the main corridor
     sim.Animate3dBox(x_len=3, y_len=1, z_len=1, color="yellow", x=0, y=y, z=0.5)
@@ -56,11 +59,11 @@ def simulate(layout: Layout, scenario: Scenario):
 
         # Robot
         if machine_left_count != 0:
-            sim.Animate3dBox(x_len=0.5, y_len=0.5, z_len=0.5, color="green", x=-1, y=y, z=2.5)
+            sim.Animate3dBox(x_len=0.5, y_len=0.5, z_len=0.5, color="green", edge_color= 'white', x=-1, y=y, z=2.5)
         else:
             False
         if machine_right_count != 0:
-            sim.Animate3dBox(x_len=0.5, y_len=0.5, z_len=0.5, color="green", x=1, y=y, z=2.5)
+            sim.Animate3dBox(x_len=0.5, y_len=0.5, z_len=0.5, color="green", edge_color= 'white', x=1, y=y, z=2.5)
         else:
             False
 
@@ -84,21 +87,28 @@ def simulate(layout: Layout, scenario: Scenario):
             #sim.Animate3dBox(x_len=0.5, y_len=0.5, z_len=1.25, color="red", x=0, y=y, z=1.625)
             #sim.Animate3dBox(x_len=0.5, y_len=0.5, z_len=1.25, color="green", x=1, y=y, z=1.625)
 
-        # Storage Area
-        sim.Animate3dBox(x_len=3, y_len=1, z_len=1, color="orange", x=0, y=y, z=0.5)
+        # Storage Area in t_corridors
+        x_len = 3
+        y_len = 1
+        z_len = 1
+        stock_volume = x_len * y_len * z_len
+        sim.Animate3dBox(x_len=x_len, y_len=y_len, z_len=z_len, color= 'orange', x=0, y=y, z=0.5)
+
+        #for productType in PRODUCT_TYPES:
+
 
         # Transversal corridors, left-right
-        x_len_left = machine_left_count * 2 + 0.5
-        x_len_right = machine_right_count * 2 + 0.5
+        x_len_left = machine_left_count * 2 + 0.38
+        x_len_right = machine_right_count * 2 + 0.38
 
         x_left = - 0.75 - x_len_left / 2
         x_right = + 0.75 + x_len_right / 2
         if machine_left_count != 0:
-            sim.Animate3dBox(x_len=x_len_left, y_len=0.5, z_len=0.25, color="green", x=x_left, y=y, z=2.5)
+            sim.Animate3dBox(x_len=x_len_left, y_len=0.25, z_len=0.25, color="green", x=x_left, y=y, z=2.5)
         else:
             False
         if machine_right_count != 0:
-            sim.Animate3dBox(x_len=x_len_right, y_len=0.5, z_len=0.25, color="green", x=x_right, y=y, z=2.5)
+            sim.Animate3dBox(x_len=x_len_right, y_len=0.25, z_len=0.25, color="green", x=x_right, y=y, z=2.5)
         else:
             False
 
@@ -106,27 +116,35 @@ def simulate(layout: Layout, scenario: Scenario):
             #sim.Animate3dBox(x_len=x_len_left, y_len=0.5, z_len=0.5, color="green", x=x_left, y=y, z=2.5)
             #sim.Animate3dBox(x_len=x_len_right, y_len=0.5, z_len=0.5, color="green", x=x_right, y=y, z=2.5)
 
+
         # Draw machine instances
         machine_num = 0
         for machine in corridor.machinesLeft:
             x = -3 - machine_num * 2
             # Down
-            sim.Animate3dBox(x_len=0.25, y_len=0.25, z_len=1.5, color="green", x=x, y=y, z=1.625)
+            sim.Animate3dBox(x_len=0.25, y_len=0.25, z_len=1.20, color="green", x=x, y=y, z=1.8)
+
             # Machine
-            sim.Animate3dBox(x_len=0.60, y_len=0.43, z_len=0.40, color="blue", x=x, y=y + 0.08, z=1)
-            sim.Animate3dBox(x_len=0.60, y_len=0.60, z_len=0.60, color="blue", x=x, y=y, z=0.5)
+            sim.Animate3dBox(x_len=0.60, y_len=0.40, z_len=0.40, color="white", x=x, y=y-0.08 , z=1)
+            sim.Animate3dBox(x_len=0.60, y_len=0.70, z_len=0.60, color="white", x=x, y=y+0.08, z=0.5)
             machine_num = machine_num + 1
 
         machine_num = 0
         for machine in corridor.machinesRight:
             x = +3 + machine_num * 2
             # Down
-            sim.Animate3dBox(x_len=0.25, y_len=0.25, z_len=1.5, color="green", x=x, y=y, z=1.625)
+            sim.Animate3dBox(x_len=0.25, y_len=0.25, z_len=1.20, color="green", x=x, y=y, z=1.8)
             # Machine
-            sim.Animate3dBox(x_len=0.60, y_len=0.43, z_len=0.40, color="blue", x=x, y=y +0.08, z=1)
-            sim.Animate3dBox(x_len=0.60, y_len=0.60, z_len=0.60, color="blue", x=x, y=y, z=0.5)
+
+            sim.Animate3dBox(x_len=0.60, y_len=0.40, z_len=0.40, color="white", x=x, y=y -0.08, z=1)
+            sim.Animate3dBox(x_len=0.60, y_len=0.70, z_len=0.60, color="white", x=x, y=y+0.08, z=0.5)
             machine_num = machine_num + 1
 
         corridor_num = corridor_num + 1
+
+
+
+
+
 
     env.run(sim.inf)
