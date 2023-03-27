baafb2c366286e5dcc4230545beef3b1f73f6439
Sammarcochiara <chiara.sammarco99@gmail.com>
2023-03-27 17:40:53 +0200

    Simulate model: main robot dumb movements

--- prototypes/advanced/core/fda/simulate.py
+++ prototypes/advanced/core/fda/simulate.py
@@ -3,74 +3,6 @@ import salabim as sim
 from .model import *
 
 
-class MainRobot(sim.Component):
-
-    def __init__(self, env: sim.Environment, y: float, z: float):
-        super().__init__()
-
-        self.env = env
-
-        self.x = 0
-
-        self.y = y
-        self.z = z
-        self.t = env.now()
-
-        self.next_y = y
-        self.next_z = z
-        self.next_t = env.now()
-
-        sim.Animate3dBox(x_len=0.5, y_len=0.5, z_len=0.5, color="red", edge_color= 'white', x=self.x, y=self.y_func, z=self.z_func)
-
-    def y_func(self, t: float):
-        if self.next_t == self.t:
-            return self.y
-        else:
-            return self.y + (self.next_y - self.y) * (t - self.t) / (self.next_t - self.t)
-
-    def z_func(self, t: float):
-        if self.next_t == self.t:
-            return self.z
-        else:
-            return self.z + (self.next_z - self.z) * (t - self.t) / (self.next_t - self.t)
-
-    def process(self):
-        duration = 5
-        while True:
-            # Move forward
-            print ("Move forward", self.env.now())
-            self.next_y = 3
-            self.next_t = self.env.now() + duration
-            yield self.hold(duration)
-            self.y = self.next_y
-            # Move down
-            print ("Move down")
-            self.next_z = 1.5
-            self.next_t = self.env.now() + duration
-            yield self.hold(duration)
-            self.z = self.next_z
-            # Move up
-            print("Move up")
-            self.next_z = 2.5
-            self.next_t = self.env.now() + duration
-            yield self.hold(duration)
-            self.z = self.next_z
-            # Move backward
-            print("Move backward")
-            self.next_y = 0
-            self.next_t = self.env.now() + duration
-            yield self.hold(duration)
-            self.y = self.next_y
-
-
-class TransversalRobot(sim.Component):
-    def __init__(self, x: float, y: float, z: float):
-        self.x = x
-        self.y = y
-        self.z = z
-        sim.Animate3dBox(x_len=0.5, y_len=0.5, z_len=0.5, color="green", edge_color= 'white', x=lambda t: x, y=y, z=lambda t: z)
-    def process(self):
-        pass
 
 def simulate(layout: Layout, scenario: Scenario):
 
@@ -105,18 +37,121 @@ def simulate(layout: Layout, scenario: Scenario):
     # Draw backbone (main corridor)
     sim.Animate3dBox(x_len=0.25, y_len=y*2.06, z_len=0.25, color="red", x=0, y=0, z=2.5)
 
-    # Down (connection robot-2 storage areas)
+    # Down (connection to the two storage areas)
     sim.Animate3dBox(x_len=0.25, y_len=0.25, z_len=1.5, color="red", x=0, y=y, z=1.625)
     sim.Animate3dBox(x_len=0.25, y_len=0.25, z_len=1.5, color="red", x=0, y=-y, z=1.625)
 
     # Robot
     #sim.Animate3dBox(x_len=0.5, y_len=0.5, z_len=0.5, color="red", edge_color= 'white', x=0, y=lambda t: t%2, z=lambda t: 2.5)
+
+
+    class MainRobot(sim.Component):
+        #movements
+        def __init__(self, env: sim.Environment, y: float, z: float):
+            super().__init__()
+
+            self.env = env
+
+            self.x = 0
+            self.y = y
+            self.z = z
+            self.t = env.now()
+
+            self.next_y = y
+            self.next_z = z
+            self.next_t = env.now()
+
+            sim.Animate3dBox(x_len=0.5, y_len=0.5, z_len=0.5, color="red", edge_color='white', x=self.x, y=self.y_func,
+                             z=self.z_func)
+
+        def y_func(self, t: float):
+            if self.next_t == self.t:
+                return self.y
+            else:
+                return self.y + (self.next_y - self.y) * (t - self.t) / (self.next_t - self.t)
+
+        def z_func(self, t: float):
+            if self.next_t == self.t:
+                return self.z
+            else:
+                return self.z + (self.next_z - self.z) * (t - self.t) / (self.next_t - self.t)
+
+
+        def process(self) :
+            duration = 1
+            corridor_count = len(layout.corridors)  # numbers of t_corridors in a certain layout
+            y_stock = 2 + corridor_count / 1.5
+
+            corridor_num = 0
+
+            for corridor in layout.corridors:  # for each corridor in the layout, define the number of machines in left and right corridor
+                y = (corridor_num + 0.5 - corridor_count / 2) * 2
+
+                #Go to the RM inventory
+                self.next_y = -y_stock
+                self.next_t = self.env.now() + duration
+                yield self.hold(duration)
+                self.y = self.next_y
+
+                # Move down in the RM inventory
+                self.next_z = 1.25
+                self.next_t = self.env.now() + duration
+                yield self.hold(duration)
+                self.z = self.next_z
+
+                # Move up
+                self.next_z = 2.5
+                self.next_t = self.env.now() + duration
+                yield self.hold(duration)
+                self.z = self.next_z
+
+                # Move to one WIP inventory
+                self.next_y = y #I'm defining the next position to reach
+                self.next_t = self.env.now() + duration
+                yield self.hold(duration)
+                self.y = self.next_y #and I'm ansking to maintain it
+
+                # Move down
+                self.next_z = 1.25
+                self.next_t = self.env.now() + duration
+                yield self.hold(duration)
+                self.z = self.next_z
+
+                # Move up
+                self.next_z = 2.5
+                self.next_t = self.env.now() + duration
+                yield self.hold(duration)
+                self.z = self.next_z
+
+
+                corridor_num = corridor_num + 1
+
+            # Go to the FP inventory
+            self.next_y = y_stock
+            self.next_t = self.env.now() + duration
+            yield self.hold(duration)
+            self.y = self.next_y
+
+            # Move down in the RM inventory
+            self.next_z = 1.25
+            self.next_t = self.env.now() + duration
+            yield self.hold(duration)
+            self.z = self.next_z
+
+            # Move up
+            self.next_z = 2.5
+            self.next_t = self.env.now() + duration
+            yield self.hold(duration)
+            self.z = self.next_z
+
+
+    #main robot position
     main_robot = MainRobot(env, 0, 2.5)
+
 
     # Storage Areas in the main corridor
     sim.Animate3dBox(x_len=3, y_len=1, z_len=1, color="yellow", x=0, y=y, z=0.5)
     sim.Animate3dBox(x_len=3, y_len=1, z_len=1, color="yellow", x=0, y=-y, z=0.5)
-
 
 
     # Draw corridor (Transversal corridors)
@@ -127,21 +162,158 @@ def simulate(layout: Layout, scenario: Scenario):
 
         y = (corridor_num + 0.5 - corridor_count/2)*2
 
+
         # Robot
+
+        class TransversalRobot_left(sim.Component):
+            def __init__(self, env: sim.Environment, x: float, y: float, z: float):
+                super().__init__()
+
+                self.env = env
+
+                self.x = x
+                self.y = y
+                self.z = z
+                self.t = env.now()
+
+                self.next_x = x
+                self.next_z = z
+                self.next_t = env.now()
+
+
+
+                sim.Animate3dBox(x_len=0.5, y_len=0.5, z_len=0.5, color="green", edge_color='white', x=self.x_func,
+                                 y=self.y, z=self.z_func)
+
+            #movements
+            def x_func(self, t: float):
+                if self.next_t == self.t:
+                    return self.x
+                else:
+                    return self.x + (self.next_x - self.x) * (t - self.t) / (self.next_t - self.t)
+
+            def z_func(self, t: float):
+                if self.next_t == self.t:
+                    return self.z
+                else:
+                    return self.z + (self.next_z - self.z) * (t - self.t) / (self.next_t - self.t)
+
+            def process(self):
+                duration = 5
+
+                x_len_left = machine_left_count * 2 + 0.38
+                x_left = -0.86 - x_len_left / 2
+
+                while True:
+                    # Move Left
+
+                    print("Move forward", self.env.now())
+                    self.next_x = 1
+                    self.next_t = self.env.now() + duration
+                    yield self.hold(duration)
+                    self.x = self.next_x
+
+                    # Move down
+                    print("Move down")
+                    self.next_z = 1.2
+                    self.next_t = self.env.now() + duration
+                    yield self.hold(duration)
+                    self.z = self.next_z
+
+                    # Move up
+                    print("Move up")
+                    self.next_z = 2.5
+                    self.next_t = self.env.now() + duration
+                    yield self.hold(duration)
+                    self.z = self.next_z
+
+                    # Move Right
+                    print("Move backward")
+                    self.next_x = 0
+                    self.next_t = self.env.now() + duration
+                    yield self.hold(duration)
+                    self.x = self.next_x
+
+        class TransversalRobot_right(sim.Component):
+            def __init__(self, env: sim.Environment, x: float, y: float, z: float):
+                super().__init__()
+
+                self.env = env
+
+                self.x = x
+                self.y = y
+                self.z = z
+                self.t = env.now()
+
+                self.next_x = x
+                self.next_z = z
+                self.next_t = env.now()
+
+                sim.Animate3dBox(x_len=0.5, y_len=0.5, z_len=0.5, color="green", edge_color='white', x=self.x_func,
+                                 y=self.y, z=self.z_func)
+
+            # movements
+            def x_func(self, t: float):
+                if self.next_t == self.t:
+                    return self.x
+                else:
+                    return self.x + (self.next_x - self.x) * (t - self.t) / (self.next_t - self.t)
+
+            def z_func(self, t: float):
+                if self.next_t == self.t:
+                    return self.z
+                else:
+                    return self.z + (self.next_z - self.z) * (t - self.t) / (self.next_t - self.t)
+
+            def process(self):
+                duration = 5
+
+                x_len_right = machine_right_count * 2 + 0.38
+                x_right = + 0.86 + x_len_right / 2
+
+                while True:
+                    # Move Left
+
+                    print("Move forward", self.env.now())
+                    self.next_x = 1  ######da risolvere
+                    self.next_t = self.env.now() + duration
+                    yield self.hold(duration)
+                    self.x = self.next_x
+
+                    # Move down
+                    print("Move down")
+                    self.next_z = 1.2
+                    self.next_t = self.env.now() + duration
+                    yield self.hold(duration)
+                    self.z = self.next_z
+
+                    # Move up
+                    print("Move up")
+                    self.next_z = 2.5
+                    self.next_t = self.env.now() + duration
+                    yield self.hold(duration)
+                    self.z = self.next_z
+
+                    # Move Right
+                    print("Move backward")
+                    self.next_x = 0
+                    self.next_t = self.env.now() + duration
+                    yield self.hold(duration)
+                    self.x = self.next_x
+
+
+
+        #if there is no machine in the corridor then we don't need the robot
         if machine_left_count != 0:
-            #sim.Animate3dBox(x_len=0.5, y_len=0.5, z_len=0.5, color="green", edge_color= 'white', x=lambda t: -1, y=y, z=lambda t: 2.5)
-            transversal_robot = TransversalRobot(-1, y, 2.5)
+            transversal_robot = TransversalRobot_left(env, -1, y, 2.5)
         else:
             False
         if machine_right_count != 0:
-            #sim.Animate3dBox(x_len=0.5, y_len=0.5, z_len=0.5, color="green", edge_color= 'white', x=lambda t: +1, y=y, z=lambda t: 2.5)
-            transversal_robot = TransversalRobot(+1, y, 2.5)
+            transversal_robot = TransversalRobot_right(env, +1, y, 2.5)
         else:
             False
 
-            #original code for robot
-            #sim.Animate3dBox(x_len=1, y_len=1, z_len=1, color="green", x=-1, y=y, z=2.5)
-            #sim.Animate3dBox(x_len=1, y_len=1, z_len=1, color="green", x=1, y=y, z=2.5)
+
 
         # Down (connection robot storage areas in t_corridors)
         sim.Animate3dBox(x_len=0.25, y_len=0.25, z_len=1.5, color="red", x=0, y=y, z=1.625)
@@ -154,27 +326,22 @@ def simulate(layout: Layout, scenario: Scenario):
         else:
             False
 
-            #original code for down
-            #sim.Animate3dBox(x_len=0.5, y_len=0.5, z_len=1.25, color="green", x=-1, y=y, z=1.625)
-            #sim.Animate3dBox(x_len=0.5, y_len=0.5, z_len=1.25, color="red", x=0, y=y, z=1.625)
-            #sim.Animate3dBox(x_len=0.5, y_len=0.5, z_len=1.25, color="green", x=1, y=y, z=1.625)
 
         # Storage Area in t_corridors
         x_len = 3
         y_len = 1
         z_len = 1
-        stock_volume = x_len * y_len * z_len
         sim.Animate3dBox(x_len=x_len, y_len=y_len, z_len=z_len, color= 'orange', x=0, y=y, z=0.5)
 
-        #for productType in PRODUCT_TYPES:
+
 
 
         # Transversal corridors, left-right
-        x_len_left = machine_left_count * 2 + 0.38
-        x_len_right = machine_right_count * 2 + 0.38
-        
-        x_left = - 0.75 - x_len_left / 2
-        x_right = + 0.75 + x_len_right / 2
+        x_len_left = machine_left_count * 2 + 0.26
+        x_len_right = machine_right_count * 2 + 0.26
+
+        x_left = -0.86 - x_len_left / 2
+        x_right = + 0.86 + x_len_right / 2
         if machine_left_count != 0:
             sim.Animate3dBox(x_len=x_len_left, y_len=0.25, z_len=0.25, color="green", x=x_left, y=y, z=2.5)
         else:
@@ -184,9 +351,8 @@ def simulate(layout: Layout, scenario: Scenario):
         else:
             False
 
-            #original code
-            #sim.Animate3dBox(x_len=x_len_left, y_len=0.5, z_len=0.5, color="green", x=x_left, y=y, z=2.5)
-            #sim.Animate3dBox(x_len=x_len_right, y_len=0.5, z_len=0.5, color="green", x=x_right, y=y, z=2.5)
+        #t_robot = TransversalRobot_left(env, x_left, y, 2.5)
+        #t_robot = TransversalRobot_right(env, x_right, y, 2.5)
 
 
         # Draw machine instances
