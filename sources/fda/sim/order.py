5611a11c0f272e0956e3d6fdd5c6016388747245
Georg Hackenberg <ghackenberg@gmail.com>
2023-06-06 08:51:07 +0200

    improve order visualization

--- sources/fda/sim/order.py
+++ sources/fda/sim/order.py
@@ -21,36 +21,42 @@ class SimOrder(sim.Component):
         print(f" - {self.order.name}:")
         for sim_job in self.sim_jobs:
             sim_job.printStatistics()
-        #orderBarChart(self.sim_jobs)
+        orderBarChart(self.order, self.sim_jobs)
 
 
-def orderBarChart(jobs: list[SimJob]):
-    categories = ['Raw', 'Intermediate', 'Finished']
-
+def orderBarChart(order: Order, jobs: list[SimJob]):
+    # Collect categories
+    categories = []
     for job in jobs:
-        raw = job.state.value.value_duration('raw')
-        intermediate = job.state.value.value_duration('intermediate')
-        finished = job.state.value.value_duration('finished')
+        for value in job.state.value.values():
+            if value not in categories:
+                categories.append(value)
+    
+    # Compute values
+    values = [0 for c in categories]
+    for job in jobs:
+        for value in job.state.value.values():
+            duration = job.state.value.value_duration(value)
+            index = categories.index(value)
+            values[index] = values[index] + duration
 
-        values = [raw, intermediate, finished]
+    # Draw chart
+    bar_width = 0.15
 
-        bar_width = 0.15
+    # Graph
+    for i in range(len(categories)):
+        plt.bar(i * bar_width, values[i], width=bar_width, label=categories[i])
 
-        # Graph
-        position = range(len(jobs))
-        for i in range(len(categories)):
-            plt.bar([p + i * bar_width for p in position], values[i], width=bar_width, label=categories[i])
+    # x Axis
+    plt.xticks([])
 
-        # x Axis
-        plt.xticks([])
+    # Labels
+    plt.xlabel('Order State')
+    plt.ylabel('State Duration')
+    plt.title(f'{order.name}')
 
-        # Labels
-        plt.xlabel('Job state duration')
-        plt.ylabel('State (%)')
-        plt.title('Order state')
+    # Legend
+    plt.legend()
 
-        # Legend
-        plt.legend()
-
-        # Print Graph
-        plt.show()
+    # Print Graph
+    plt.show()
